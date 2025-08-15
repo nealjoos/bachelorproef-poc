2e972deb948ee134191d46dcdf161038021f95a8
Neal Joos <neal.joos@student.hogent.be>
2025-08-15 18:53:18 +0200

    grafana, syslog-ng

--- scripts/student.sh
+++ scripts/student.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 # Replace the addresses line with dhcp4: true
-sed -i '/eth1:/,/^[^ ]/ { 
+sed -i '/eth1:/,/^[^ ]/ {
     /addresses:/d
-    /-192\.168\.60\.11\/24/d
+    /^  - /d
     /eth1:/a\  dhcp4: true
-}' 50-vagrant.yaml
+}' /etc/netplan/50-vagrant.yaml
+
+# Replace XKBLAYOUT line to set Belgian layout
+sudo sed -i 's/^XKBLAYOUT=.*/XKBLAYOUT="be"/' /etc/default/keyboard
